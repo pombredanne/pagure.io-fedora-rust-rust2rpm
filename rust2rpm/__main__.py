c282aeab49877400795714b8d26a023ca0fb3480
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 23:42:00 +0000

    Adding tarfile member sanitization to extractall()

--- rust2rpm/__main__.py
+++ rust2rpm/__main__.py
@@ -137,7 +137,26 @@ def toml_from_crate(cratef, crate, version):
             for n in archive.getnames():
                 if not os.path.abspath(os.path.join(target_dir, n)).startswith(target_dir):
                     raise Exception("Unsafe filenames!")
-            archive.extractall(target_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, target_dir)
         toml_relpath = f"{crate}-{version}/Cargo.toml"
         toml = f"{tmpdir}/{toml_relpath}"
         if not os.path.isfile(toml):
